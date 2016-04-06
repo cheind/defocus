fee86bea3b5ac423b915c4dd2263fbecae932671
Christoph Heindl <christoph.heindl@gmail.com>
2016-04-06 15:39:15 +0200

    Updated to new API

--- examples/defocus_statistics.cpp
+++ examples/defocus_statistics.cpp
@@ -22,21 +22,16 @@
 #include <Eigen/Sparse>
 #include <Eigen/Dense>
 
-typedef std::vector<cv::Point2f> OpenCVFeatures;
-typedef std::vector< OpenCVFeatures > OpenCVFeaturesPerFrame;
 
-Eigen::MatrixXd computeFeatureDistanceMatrix(const OpenCVFeaturesPerFrame &f)
+Eigen::MatrixXd computeFeatureDistanceMatrix(const Eigen::MatrixXd &f)
 {
-    Eigen::MatrixXd d(f[0].size(), f.size());
+    Eigen::MatrixXd d(f.cols(), f.rows() / 2);
 
     // For each feature
-    for (size_t i = 0; i < f[0].size(); ++i) {
-        cv::Vec2f ref = f[0][i];
-
+    for (Eigen::DenseIndex i = 0; i < f.cols(); ++i) {
         // In each frame
-        for (size_t j = 0; j < f.size(); ++j) {
-            cv::Vec2f t = f[j][i];
-            d(i, j) = sqrt((ref - t).dot((ref - t)));
+        for (Eigen::DenseIndex j = 0; j < f.rows() / 2; ++j) {
+            d(i, j) = (f.block<2, 1>(0, i) - f.block<2, 1>(j * 2, i)).norm();
         }
     }
 
@@ -49,15 +44,15 @@ void writeFeatureDistanceMatrixToFile(const char *path, const Eigen::MatrixXd &m
     ofs << m.format(Eigen::IOFormat(Eigen::StreamPrecision, Eigen::DontAlignCols, " ", "\n"));
 }
 
-cv::Mat generateFeatureMaxMovementImage(cv::Mat &ref, const OpenCVFeatures &f, const Eigen::MatrixXd &m)
+cv::Mat generateFeatureMaxMovementImage(cv::Mat &ref, const Eigen::MatrixXd &f, const Eigen::MatrixXd &m)
 {
     cv::Mat tmp = ref.clone();
 
     Eigen::VectorXd maxD = m.rowwise().mean();
 
     for (Eigen::DenseIndex i = 0; i < maxD.rows(); ++i) {
-        std::cout << (int)maxD(i) << std::endl;
-        cv::circle(tmp, f[i], (int)maxD(i), cv::Scalar(0, 255, 0));
+        cv::Point2d p(f(0, i), f(1, i));
+        cv::circle(tmp, p, (int)maxD(i), cv::Scalar(0, 255, 0));
     }
 
     return tmp;
@@ -91,47 +86,37 @@ int main(int argc, char **argv) {
     Eigen::Matrix3d invk = k.inverse();
 
     // Detect trackable features in reference frame
-    cv::Mat ref, refF, refGray, refLab, gray;
+    cv::Mat ref;
     vc >> ref;
     
-    cv::cvtColor(ref, refGray, CV_BGR2GRAY);
-    OpenCVFeaturesPerFrame featuresPerFrame;
-    
-    std::vector<cv::Point2f> corners;
-    defocus::findFeatureInImage(refGray, corners);
-    std::vector<uchar> status(corners.size(), 1);
-    featuresPerFrame.push_back(corners);
+    defocus::SmallMotionTracker tracker;
+    tracker.setMaxError(5.0);
+    tracker.initializeFromReferenceFrame(ref);
 
     cv::Mat f;
     while (vc.grab()) {
         vc.retrieve(f);
-        cv::cvtColor(f, gray, CV_BGR2GRAY);
 
-        std::vector<cv::Point2f> loc;
+        defocus::SmallMotionTracker::CVFrameResult r = tracker.addFrame(f);
 
-        defocus::trackFeatures(refGray, corners, gray, loc, status, 5);
-        featuresPerFrame.push_back(loc);
-        
-        for (size_t i = 0; i < status.size(); ++i) {
-            if (status[i]) {
-                cv::circle(f, loc[i], 2, cv::Scalar(0, 255, 0));
+        int count = 0;
+        for (size_t i = 0; i < r.second.size(); ++i) {
+            if (r.second[i]) {
+                cv::circle(f, r.first[i], 2, cv::Scalar(0, 255, 0));
             }
         }
-        
+
         cv::Mat tmp;
         cv::resize(f, tmp, cv::Size(), 0.5, 0.5);
         cv::imshow("track", tmp);
         cv::waitKey(10);
     }
 
-    for (Eigen::DenseIndex c = 0; c < featuresPerFrame.size(); ++c) {
-        defocus::removeByStatus(featuresPerFrame[c], status);
-    }    
-
-    Eigen::MatrixXd dists = computeFeatureDistanceMatrix(featuresPerFrame);
+    Eigen::MatrixXd features = tracker.trackedFeaturesPerFrame();
+    Eigen::MatrixXd dists = computeFeatureDistanceMatrix(features);
 
     writeFeatureDistanceMatrixToFile("featuredists.csv", dists);
-    cv::Mat featureMovementImage = generateFeatureMaxMovementImage(ref, featuresPerFrame[0], dists);
+    cv::Mat featureMovementImage = generateFeatureMaxMovementImage(ref, features.topRows(2), dists);
     cv::imwrite("featuremovement.png", featureMovementImage);
 
 }
